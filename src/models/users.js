4808051418c80e0b6f4c617fc0d5d8906642eea6
Alexandre Lerosier <alex.lerosier@gmail.com>
2024-03-22 11:40:40 +0100

    feat : signIn and signUp

---
+++ src/models/users.js
@@ -0,0 +1,23 @@
+import useSupabase from "./../composables/supabase.js";
+
+export async function signUp(email, password) {
+    const { supabase } = useSupabase();
+
+    if (password.length < 8) {
+        return;
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password
+    });
+}
+
+export async function signIn(email, password) {
+    const { supabase } = useSupabase();
+
+    const { data, error } = await supabase.auth.signIn({
+        email: email,
+        password: password
+    });
+}
