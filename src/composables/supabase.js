c507830eb482be9f0a012309404931d02211aff3
Alexandre Lerosier <alex.lerosier@gmail.com>
2024-03-22 10:30:33 +0100

    fix : supabase whith .env

--- src/composables/supabase.js
+++ src/composables/supabase.js
@@ -1,12 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 export default function useSupabase() {
-    const connexion = {
-        url: "https://ulxbtgfcimyvvpombuby.supabase.co",
-        anon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVseGJ0Z2ZjaW15dnZwb21idWJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEwOTY1MTMsImV4cCI6MjAyNjY3MjUxM30.thT09WvqlyzrzFRLuaFHz9JPfpkGTKlEXFL0wt9hG08"
-    }
+    const url = import.meta.env.VITE_SUPABASE_URL; 
+    const anon = import.meta.env.VITE_SUPABASE_ANON;
 
-    const supabase = createClient(connexion.url, connexion.anon);
+    const supabase = createClient(url, anon);
 
     return { supabase }
 }
