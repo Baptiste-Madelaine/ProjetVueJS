deb5c63dbcabb58fb81c12300700dc4c74977128
Alexandre Lerosier <alex.lerosier@gmail.com>
2024-03-22 11:29:53 +0100

    feat : can index, store and get

--- src/models/matchs.js
+++ src/models/matchs.js
@@ -1,1 +1,36 @@
+import useSupabase from "./../composables/supabase.js";
+
+export class Match {
+    name;
+    members;
+    leader;
+}
+
+
+export async function index() {
+    const { supabase } = useSupabase();
+
+    const { data, error } = await supabase.from("matchs").select();
+
+    return data;
+}
+
+export async function store(match) {
+    if (!match instanceof Match) {
+        return;
+    }
+
+    const { supabase } = useSupabase();
+    const { data } = await supabase.from("matchs").insert([match]);
+
+    return data;
+}
+
+export async function getMatch($id) {
+    const { supabase } = useSupabase();
+    const { data, error } = await supabase.from("matchs").select().eq("id", $id).first();
+
+    return data;
+}
+
 
