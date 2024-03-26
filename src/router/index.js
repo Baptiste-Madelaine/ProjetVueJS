0e51ab2a3920bb5a364cab72a5a81a689d342006
Hugo <hugo.sefrioui@etu-unicaen.fr>
2024-03-26 16:15:36 +0100

    Fix Merge Conflicts

--- src/router/index.js
+++ src/router/index.js
@@ -13,24 +13,17 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
     {
-<<<<<<< HEAD
       path: '/signup',
       name: 'signup',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/SignUpView.vue')
-=======
+    },
+    {
       path: '/rankings',
       name: 'rankings',
       component: RankingsView
->>>>>>> dev
     }
   ]
 })
