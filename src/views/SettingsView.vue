62835b361548aa8c5c4c176c5deaa93fe812a6a0
hugo <hugo.sefrioui@etu.unicaen.fr>
2024-03-28 11:25:44 +0100

    Setting SettingsView

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -9,7 +9,7 @@
     const userStore = useUserStore()
     const { id, name} = storeToRefs(userStore);
 
-    const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+    const items = ref([{ message: 'member1' }, { message: 'member2' }])
     //const team = await getTeam(id);
 </script>
 <template>
@@ -21,8 +21,10 @@
         <input class="text-black p-1" type="text" placeholder="Team Name" v-model="email" required>
 
         <h1> Members </h1>
+        <li> firstname </li>
         <li v-for="item in items">
             {{ item.message }}
+            <button class="p-2 bg-blue-600 border-black border-4 rounded-full" @click=""> X </button>
         </li>
         <button class="p-2 bg-blue-600 border-black border-4 rounded-full" @click=""> Add a member + </button>
     </div>
